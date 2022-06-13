4388f846cc087f9086f4615b4b84e5b0aa2716bf
Danny McGee <dannymcgee@gmail.com>
2022-06-13 10:41:21 -0400

    Fix scope for doc comments; include sassdoc grammar

--- packages/grammar/src/lib/comment.ts
+++ packages/grammar/src/lib/comment.ts
@@ -3,19 +3,32 @@ import { TMGrammarScope } from "@vscode-devkit/grammar";
 export const comment: TMGrammarScope = {
 	patterns: [
 		{
-			name: "comment.documentation.scss",
+			name: "comment.block.documentation.scss",
 			begin: /\/\/\//,
+			beginCaptures: {
+				0: { name: "punctuation.definition.comment.scss" },
+			},
 			end: /(?=$)/,
+			patterns: [{ include: "source.sassdoc" }],
 		},
 		{
 			name: "comment.line.scss",
 			begin: /\/\//,
+			beginCaptures: {
+				0: { name: "punctuation.definition.comment.scss" },
+			},
 			end: /(?=$)/,
 		},
 		{
 			name: "comment.block.scss",
 			begin: /\/\*/,
+			beginCaptures: {
+				0: { name: "punctuation.definition.comment.begin.scss" },
+			},
 			end: /\*\//,
+			endCaptures: {
+				0: { name: "punctuation.definition.comment.end.scss" },
+			},
 		},
 	],
 };
